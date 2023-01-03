529e1911d85c6027164dfc4b9adc5132ac2331a0
John Casey McDonough <johncaseymcd@gmail.com>
2023-01-03 12:33:05 -0800

    Added some Kubernetes stuff. Next step: Migrate docker images using Helm. Now playing: Stamp On It by GOT the beat

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ const getClient = () => {
   const params = {
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
-    password: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
   };
