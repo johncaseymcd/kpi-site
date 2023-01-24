66f0d8a58348ba6ddce7b8713d856229989f430b
John Casey McDonough <johncaseymcd@gmail.com>
2023-01-24 12:08:28 -0800

    Finished scaffolding admin tests and refactoring admin routes. Now playing: Tangelo by Red Hot Chili Peppers

---
+++ server/tests/admins.test.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-undef */
+const request = require("supertest");
+const { app } = require("../index");
+const req = request(app);
+
+describe("POST /admins/new", () => {
+  it("should create a new admin", async () => {
+    const memberId = 1;
+    const res = await req.post(`/admins/new/${memberId}`).send({
+      memberId: memberId,
+      role: "Lead",
+      platform: "Website",
+      section: "KPI",
+    });
+
+    expect(res.statusCode).toBe(201);
+  });
+});
+
+describe("GET /admins/new/:memberId", () => {
+  it("should return a 200 with a message", async () => {
+    const memberId = 1;
+    const res = await req.get(`/admins/new/${memberId}`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("GET /admins/:page", () => {
+  it("should return a list of all the admins", async () => {
+    const res = await req.get("/admins/1");
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("GET /admins/:role/:page", () => {
+  it("should return a list of admins with the given role", async () => {
+    const role = "Lead";
+    const res = await req.get(`/admins/${role}/1`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("GET /admins/:platform/:page", () => {
+  it("should return a list of admins on the given platform", async () => {
+    const platform = "Website";
+    const res = await req.get(`/admins/${platform}/1`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("GET /admins/:section/:page", () => {
+  it("should return a list of admins in the given section", async () => {
+    const section = "KPI";
+    const res = await req.get(`/admins/${section}/1`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("GET /admins/:memberId", () => {
+  it("should return a single admin by ID", async () => {
+    const memberId = 1;
+    const res = await req.get(`/admins/${memberId}`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("PUT /admins/delete/:memberId", () => {
+  it("should set the is_deleted flag on the admin to true", async () => {
+    const memberId = 1;
+    const res = await req.put(`/admins/delete/${memberId}`);
+
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("PUT /admins/update/:memberId", () => {
+  it("should update the info for the admin", async () => {
+    const memberId = 1;
+    const res = await req.put(`/admins/update/${memberId}`).send({
+      memberId: 1,
+      role: "Admin",
+      platform: "Website",
+      section: "KPI",
+    });
+
+    expect(res.statusCode).toBe(200);
+  });
+});
--- server/tests/members.test.js
+++ server/tests/members.test.js
@@ -5,41 +5,39 @@ const req = request(app);
 
 describe("POST /members/new", () => {
   it("should create a new member", async () => {
-    const res = await req
-      .post("/members/new")
-      .send({
-        firstName: "Nicholas",
-        middleName: "Patrick",
-        lastName: "Sullivan",
-        nickname: "Nick",
-        email: "npsullivan26@gmail.com",
-        phoneNumber: "442-800-3999",
-        streetAddress: "2025 US Hwy 18",
-        unitNumber: "Ste 430-147",
-        city: "Apple Valley",
-        state: "California",
-        zip: 92307,
-        foundMethod: "Business Card",
-        pronouns: "He/Him, They/Them",
-        neopronouns: null,
-        birthday: new Date(1994, 1, 6),
-        twitterHandle: "@6tonguppy",
-        instagramHandle: "@6tonguppy",
-        dietaryRestrictions: "none",
-        hasAttendedEvent: true,
-        isAdmin: true,
-        isOnMailingList: true,
-        isOver18: true,
-        isOver21: true,
-      });
-    
+    const res = await req.post("/members/new").send({
+      firstName: "Nicholas",
+      middleName: "Patrick",
+      lastName: "Sullivan",
+      nickname: "Nick",
+      email: "npsullivan26@gmail.com",
+      phoneNumber: "442-800-3999",
+      streetAddress: "2025 US Hwy 18",
+      unitNumber: "Ste 430-147",
+      city: "Apple Valley",
+      state: "California",
+      zip: 92307,
+      foundMethod: "Business Card",
+      pronouns: "He/Him, They/Them",
+      neopronouns: null,
+      birthday: new Date(1994, 1, 6),
+      twitterHandle: "@6tonguppy",
+      instagramHandle: "@6tonguppy",
+      dietaryRestrictions: "none",
+      hasAttendedEvent: true,
+      isAdmin: true,
+      isOnMailingList: true,
+      isOver18: true,
+      isOver21: true,
+    });
+
     expect(res.statusCode).toBe(201);
     expect(res.body).toHaveProperty("post");
   });
 });
 
 describe("GET /members/new", () => {
-  it("should return a 200 with a message", async() => {
+  it("should return a 200 with a message", async () => {
     const res = await req.get("/members/new");
 
     expect(res.statusCode).toBe(200);
@@ -47,7 +45,7 @@ describe("GET /members/new", () => {
 });
 
 describe("GET /members/all/:page", () => {
-  it("should return a list of members", async() => {
+  it("should return a list of members", async () => {
     const res = await req.get("/members/all/1");
 
     expect(res.statusCode).toBe(200);
@@ -55,7 +53,7 @@ describe("GET /members/all/:page", () => {
 });
 
 describe("GET /members/:city/:page", () => {
-  it("should return a list of members in the given city", async() => {
+  it("should return a list of members in the given city", async () => {
     const city = "Indianapolis";
     const res = await req.get(`/members/${city}/1`);
 
@@ -64,7 +62,7 @@ describe("GET /members/:city/:page", () => {
 });
 
 describe("GET /members/:zip/:page", () => {
-  it("should return a list of members in the given zip code", async() => {
+  it("should return a list of members in the given zip code", async () => {
     const zip = 46202;
     const res = await req.get(`/members/${zip}/1`);
 
@@ -73,7 +71,7 @@ describe("GET /members/:zip/:page", () => {
 });
 
 describe("GET /members/:foundMethod/:page", () => {
-  it("should return a list of members who found KPI in the same way", async() => {
+  it("should return a list of members who found KPI in the same way", async () => {
     const foundMethod = encodeURI("Business Card");
     const res = await req.get(`/members/${foundMethod}/1`);
 
@@ -82,7 +80,7 @@ describe("GET /members/:foundMethod/:page", () => {
 });
 
 describe("GET /members/birthday/:page", () => {
-  it("should return a list of members whose birthdays occur this week", async() => {
+  it("should return a list of members whose birthdays occur this week", async () => {
     const res = await req.get("/members/birthday/page");
 
     expect(res.statusCode).toBe(200);
@@ -91,7 +89,7 @@ describe("GET /members/birthday/:page", () => {
 });
 
 describe("GET /members/events/:status/:page", () => {
-  it("should return a list of members who have (or have not) attended an event", async() => {
+  it("should return a list of members who have (or have not) attended an event", async () => {
     const status = true;
     const res = await req.get(`/members/events/${status}/1`);
 
@@ -100,7 +98,7 @@ describe("GET /members/events/:status/:page", () => {
 });
 
 describe("GET /members/mailto/:page", () => {
-  it("should return a list of members who are on the mailing list", async() => {
+  it("should return a list of members who are on the mailing list", async () => {
     const res = await req.get("/members/mailto/1");
 
     expect(res.statusCode).toBe(200);
@@ -108,7 +106,7 @@ describe("GET /members/mailto/:page", () => {
 });
 
 describe("GET /members/over18/:page", () => {
-  it("should return a list of members who are over 18 years old", async() => {
+  it("should return a list of members who are over 18 years old", async () => {
     const res = await req.get("/members/over18/1");
 
     expect(res.statusCode).toBe(200);
@@ -116,7 +114,7 @@ describe("GET /members/over18/:page", () => {
 });
 
 describe("GET /members/over21/:page", () => {
-  it("should return a list of members who are over 21 years old", async() => {
+  it("should return a list of members who are over 21 years old", async () => {
     const res = await req.get("/members/over21/1");
 
     expect(res.statusCode).toBe(200);
@@ -124,7 +122,7 @@ describe("GET /members/over21/:page", () => {
 });
 
 describe("GET /members/:memberId", () => {
-  it("should return a single member by their ID", async() => {
+  it("should return a single member by their ID", async () => {
     const memberId = 1;
     const res = await req.get(`/members/${memberId}`);
 
@@ -133,7 +131,7 @@ describe("GET /members/:memberId", () => {
 });
 
 describe("PUT /members/delete/:memberId", () => {
-  it("should set the is_deleted flag on the member to TRUE", async() => {
+  it("should set the is_deleted flag on the member to TRUE", async () => {
     const memberId = 1;
     const res = await req.put(`/members/delete/${memberId}`);
 
@@ -142,9 +140,33 @@ describe("PUT /members/delete/:memberId", () => {
 });
 
 describe("PUT /members/update/:memberId", () => {
-  it("should update the info for the member with the given ID", async() => {
+  it("should update the info for the member with the given ID", async () => {
     const memberId = 1;
-    const res = await req.put(`/members/update/${memberId}`);
+    const res = await req.put(`/members/update/${memberId}`).send({
+      firstName: "Nicholas",
+      middleName: "Changed",
+      lastName: "Sullivan",
+      nickname: "Nick",
+      email: "npsullivan26@gmail.com",
+      phoneNumber: "442-800-3999",
+      streetAddress: "2025 US Hwy 18",
+      unitNumber: "Ste 430-147",
+      city: "Apple Valley",
+      state: "California",
+      zip: 92307,
+      foundMethod: "Business Card",
+      pronouns: "He/Him, They/Them",
+      neopronouns: null,
+      birthday: new Date(1994, 1, 6),
+      twitterHandle: "@6tonguppy",
+      instagramHandle: "@6tonguppy",
+      dietaryRestrictions: "none",
+      hasAttendedEvent: true,
+      isAdmin: true,
+      isOnMailingList: true,
+      isOver18: true,
+      isOver21: true,
+    });
 
     expect(res.statusCode).toBe(500);
   });
